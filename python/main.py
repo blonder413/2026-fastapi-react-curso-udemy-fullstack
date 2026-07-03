751c3535bc1a91c78e2bd201e425f5ba590e2028
Jonathan Morales <blonder413@gmail.com>
2026-07-03 06:52:17 -0500

    fast-api: json response

--- python/main.py
+++ python/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
@@ -6,3 +7,8 @@ app = FastAPI()
 @app.get("/")
 def index():
     return {"message": "Hello World"}
+
+
+@app.get("/json-response")
+def json_response():
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"message": "It's work"})
