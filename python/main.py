980a74aa2ddd40226714cd047693f71129916948
Jonathan Morales <blonder413@gmail.com>
2026-07-04 18:19:18 -0500

    fast-api: custom 404

--- python/main.py
+++ python/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, Request
 from fastapi.responses import JSONResponse
 
 from router.ejemplo_router import router as ejemplo_router
@@ -13,6 +13,17 @@ def index():
 
 @app.get("/json-response")
 def json_response():
-    return JSONResponse(status_code=status.HTTP_200_OK, content={"message": "It's work"})
+    return JSONResponse(
+        status_code=status.HTTP_200_OK, content={"message": "It's work"}
+    )
+
+
+@app.exception_handler(status.HTTP_404_NOT_FOUND)
+async def not_found_exception_handler(request: Request, exc: Exception):
+    return JSONResponse(
+        status_code=status.HTTP_404_NOT_FOUND,
+        content={"status": status.HTTP_404_NOT_FOUND, "message": "Not Found"},
+    )
+
 
 app.include_router(ejemplo_router)
