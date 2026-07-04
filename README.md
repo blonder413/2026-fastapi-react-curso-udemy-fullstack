4c86f30812bdbc2456d6bc5e0c2aea79c65fa26e
Jonathan Morales <blonder413@gmail.com>
2026-07-04 11:56:36 -0500

    fast-api: restfull endpoints

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Aprendiendo fastapi y react con un curso de udemy
 # Levantar proyecto
 
 ```bash
-docker exec -it curso-fastapi-python uvicorn main:app --host 0.0.0.0 --port 8051 --reload
+docker exec -it curso-fastapi-python uvicorn main:app --host 0.0.0.0 --port 8050 --reload
 ```
--- python/router/ejemplo_router.py
+++ python/router/ejemplo_router.py
@@ -10,3 +10,24 @@ async def index():
     return JSONResponse(
         status_code=status.HTTP_200_OK, content={"message": "Hello World"}
     )
+
+
+@router.post("/")
+async def create():
+    return JSONResponse(
+        status_code=status.HTTP_201_CREATED, content={"status": "ok", "message": "POST"}
+    )
+
+
+@router.put("/")
+async def update():
+    return JSONResponse(
+        status_code=status.HTTP_200_OK, content={"status": "ok", "message": "PUT"}
+    )
+
+
+@router.delete("/")
+async def destroy():
+    return JSONResponse(
+        status_code=status.HTTP_200_OK, content={"status": "ok", "message": "DELETE"}
+    )
