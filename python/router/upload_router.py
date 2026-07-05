5a7c9f0650272e04d026c880f34601531ca1c14a
Jonathan Morales <blonder413@gmail.com>
2026-07-05 09:52:24 -0500

    fast-api: validate mimetype

--- python/router/upload_router.py
+++ python/router/upload_router.py
@@ -15,12 +15,24 @@ async def index(id: Annotated[int, Form()]):
 
 @router.post("/file")
 async def file(file: UploadFile):
+
+    if file.content_type == "image/jpeg":
+        extension = "jpg"
+    elif file.content_type == "image/png":
+        extension = "png"
+    else:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={"status": status.HTTP_400_BAD_REQUEST, "response": {}},
+        )
+
     return JSONResponse(
         status_code=status.HTTP_201_CREATED,
         content={
             "status": status.HTTP_201_CREATED,
             "response": {
                 "content-type": file.content_type,
+                "extension": extension,
                 "filename": f"{file.filename}",
                 "size": file.size,
             },
