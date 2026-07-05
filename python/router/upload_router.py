d211b60418d385609c1bf74cd3924e0944eb1c67
Jonathan Morales <blonder413@gmail.com>
2026-07-05 09:29:03 -0500

    fast-api: send files using form-data

--- python/router/upload_router.py
+++ python/router/upload_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status, Form
+from fastapi import APIRouter, status, Form, UploadFile
 from fastapi.responses import JSONResponse
 from typing import Annotated
 
@@ -6,8 +6,23 @@ router = APIRouter(prefix="/upload", tags=["Upload"])
 
 
 @router.post("/")
-async def upload(id: Annotated[int, Form()]):
+async def index(id: Annotated[int, Form()]):
     return JSONResponse(
         status_code=status.HTTP_201_CREATED,
-        content={"status": status.HTTP_201_CREATED, "message": f"File uploaded: {id}"},
+        content={"status": status.HTTP_201_CREATED, "message": f"form-data: {id}"},
+    )
+
+
+@router.post("/file")
+async def file(file: UploadFile):
+    return JSONResponse(
+        status_code=status.HTTP_201_CREATED,
+        content={
+            "status": status.HTTP_201_CREATED,
+            "response": {
+                "content-type": file.content_type,
+                "filename": f"{file.filename}",
+                "size": file.size,
+            },
+        },
     )
