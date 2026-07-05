dac9fc0837a1b6e2e6dcbe5f74bedb2ac63359ac
Jonathan Morales <blonder413@gmail.com>
2026-07-05 10:19:47 -0500

    fast-api: upload file locally

--- python/router/upload_router.py
+++ python/router/upload_router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, status, Form, UploadFile
 from fastapi.responses import JSONResponse
 from typing import Annotated
+import os
+import uuid
 
 router = APIRouter(prefix="/upload", tags=["Upload"])
 
@@ -34,6 +36,44 @@ async def file(file: UploadFile):
                 "content-type": file.content_type,
                 "extension": extension,
                 "filename": f"{file.filename}",
+                "size": file.size,
+            },
+        },
+    )
+
+
+@router.post("/local")
+async def local(file: UploadFile):
+    if file.content_type == "image/jpeg":
+        extension = "jpg"
+    elif file.content_type == "image/png":
+        extension = "png"
+    else:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "status": {
+                    "status_code": status.HTTP_400_BAD_REQUEST,
+                    "message": "Invalid file",
+                },
+                "response": {},
+            },
+        )
+
+    name = f"{uuid.uuid4()}.{extension}"
+    file_location = os.path.join("uploads", name)
+    with open(file_location, "wb") as buffer:
+        buffer.write(await file.read())
+
+    return JSONResponse(
+        status_code=status.HTTP_201_CREATED,
+        content={
+            "status": status.HTTP_201_CREATED,
+            "response": {
+                "content-type": file.content_type,
+                "extension": extension,
+                "filename": file.filename,
+                "name": name,
                 "size": file.size,
             },
         },
