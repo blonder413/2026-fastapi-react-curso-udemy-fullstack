11f15f4c7085358221e79903eb62ff63ef7b6a17
Jonathan Morales <blonder413@gmail.com>
2026-07-05 18:27:17 -0500

    fast-api: boto3 connection

--- python/router/upload_router.py
+++ python/router/upload_router.py
@@ -3,8 +3,23 @@ from fastapi.responses import JSONResponse, FileResponse
 from typing import Annotated
 import os
 import uuid
+import boto3
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 router = APIRouter(prefix="/upload", tags=["Upload"])
+
+
+s3_client = boto3.client(
+    "s3",
+    region_name=os.getenv("AWS_REGION"),
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    endpoint_url=os.getenv("AWS_SECRET_ACCESS_URL"),
+)
+S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
 
 
 @router.post("/")
