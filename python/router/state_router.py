2623c3522926efede2d4ed9ebf6533345596ea96
Jonathan Morales <blonder413@gmail.com>
2026-07-08 11:39:32 -0500

    fast-api: list states

--- python/router/state_router.py
+++ python/router/state_router.py
@@ -1,4 +1,19 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Depends
 from fastapi.responses import JSONResponse
+from database import get_session
+from sqlmodel import Session
+from models.models import Estado
 
 router = APIRouter(prefix="/state", tags=["State"])
+
+
+@router.get("/", response_model=list[Estado])
+async def index(session: Session = Depends(get_session)):
+    data = session.query(Estado).all()
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={
+            "status": {"status_code": status.HTTP_200_OK},
+            "response": [value.model_dump() for value in data],
+        },
+    )
