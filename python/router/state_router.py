01f58bdba2ead80192e1316ae281f1390ffb555e
Jonathan Morales <blonder413@gmail.com>
2026-07-08 12:10:22 -0500

    fast-api: find one

--- python/router/state_router.py
+++ python/router/state_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, HTTPException
 from fastapi.responses import JSONResponse
 from database import get_session
 from sqlmodel import Session
@@ -16,5 +16,21 @@ async def index(session: Session = Depends(get_session)):
         content={
             "status": {"status_code": status.HTTP_200_OK},
             "response": [value.model_dump() for value in data],
+        },
+    )
+
+
+@router.get("/{id}")
+async def show(id: int, session: Session = Depends(get_session)):
+    data = session.get(Estado, id)
+
+    if not data:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found")
+
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={
+            "status": {"status_code": status.HTTP_200_OK},
+            "response": data.model_dump(),
         },
     )
