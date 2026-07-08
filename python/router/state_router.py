b798392696db2e4be119601ec5cdedfd8af0847e
Jonathan Morales <blonder413@gmail.com>
2026-07-08 11:53:06 -0500

    fast-api: order by

--- python/router/state_router.py
+++ python/router/state_router.py
@@ -3,13 +3,14 @@ from fastapi.responses import JSONResponse
 from database import get_session
 from sqlmodel import Session
 from models.models import Estado
+from sqlalchemy import desc
 
 router = APIRouter(prefix="/state", tags=["State"])
 
 
 @router.get("/", response_model=list[Estado])
 async def index(session: Session = Depends(get_session)):
-    data = session.query(Estado).all()
+    data = session.query(Estado).order_by(desc(Estado.id)).all()
     return JSONResponse(
         status_code=status.HTTP_200_OK,
         content={
