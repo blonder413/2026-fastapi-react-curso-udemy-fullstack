318a38451fc2d24b33daec389c9e91a890fb027e
Jonathan Morales <blonder413@gmail.com>
2026-07-11 17:23:54 -0500

    fast-api(category): dto

---
+++ python/router/dto/category_dto.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, model_validator
+
+
+class CategoryDto(BaseModel):
+    nombre: str
+
+    @model_validator(mode="after")
+    def validate_nombre(self):
+        if not self.nombre or len(self.nombre.strip()) < 3:
+            raise ValueError("Min length: 3 characters")
+        return self
