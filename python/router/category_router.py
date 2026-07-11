053dd4b0b7bbcb4b17421f38b1b2548cbb429327
Jonathan Morales <blonder413@gmail.com>
2026-07-11 17:35:03 -0500

    fast-api(category): findAll

--- python/router/category_router.py
+++ python/router/category_router.py
@@ -1,4 +1,24 @@
 from fastapi import APIRouter, status, Depends, HTTPException
 from fastapi.responses import JSONResponse
 
+from database import get_session
+from sqlalchemy import desc
+from sqlmodel import Session
+
+from .dto.category_dto import CategoryDto
+from interfaces.interfaces import GenericInterface
+from models.models import Category
+
 router = APIRouter(prefix="/category", tags=["Category"])
+
+
+@router.get("/", response_model=list[Category])
+async def index(session: Session = Depends(get_session)):
+    data = session.query(Category).order_by(desc(Category.id)).all()
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={
+            "status": {"status_code": status.HTTP_200_OK, "message": "Records Found"},
+            "response": [value.model_dump() for value in data],
+        },
+    )
