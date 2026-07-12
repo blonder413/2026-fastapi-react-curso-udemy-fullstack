a9fce93dd9ca31b75d5afcfab0a43e684a861f5f
Jonathan Morales <blonder413@gmail.com>
2026-07-12 09:47:11 -0500

    fast-api(category): find one

--- python/router/category_router.py
+++ python/router/category_router.py
@@ -22,3 +22,26 @@ async def index(session: Session = Depends(get_session)):
             "response": [value.model_dump() for value in data],
         },
     )
+
+
+@router.get("/{id}", response_model=list[Category])
+async def index(id: int, session: Session = Depends(get_session)):
+    data = session.get(Category, id)
+    if not data:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={
+                "status": {
+                    "status_code": status.HTTP_404_NOT_FOUND,
+                    "message": "Not found",
+                },
+                "response": {},
+            },
+        )
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={
+            "status": {"status_code": status.HTTP_200_OK, "message": "Record Found"},
+            "response": data.model_dump(),
+        },
+    )
