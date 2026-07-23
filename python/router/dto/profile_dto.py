8648af8b83f8a75caa94a7fdb4e99de25cefe53d
Jonathan Morales <blonder413@gmail.com>
2026-07-23 11:45:57 -0500

    fast-api(profile): dto

---
+++ python/router/dto/profile_dto.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, model_validator
+
+
+class ProfileDto(BaseModel):
+    name: str
+
+    @model_validator(mode="after")
+    def validate_name(self):
+        if not self.name or len(self.name.strip()) < 3:
+            raise ValueError("Min length: 3 characters")
+        return self
