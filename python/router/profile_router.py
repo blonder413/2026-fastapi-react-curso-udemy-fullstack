7095695c0fcf35d2002bbbd5a68adefd55ab35e6
Jonathan Morales <blonder413@gmail.com>
2026-07-25 08:22:22 -0500

    fast-api(profile): show

--- python/router/profile_router.py
+++ python/router/profile_router.py
@@ -1,3 +1,37 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.responses import JSONResponse
+
+from database import get_session
+from sqlmodel import Session
+from typing import Annotated
+
+from interfaces.Response import ResponseInterface
+from models.models import Profile
 
 router = APIRouter(prefix="/profile", tags=["Profile"])
+
+
+@router.get("/", response_model=ResponseInterface[list[Profile]])
+async def index(session: Annotated[Session, Depends(get_session)]):
+    data = session.query(Profile).order_by(Profile.id.desc()).all()
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={
+            "status": {"status_code": status.HTTP_200_OK, "message": "Records Found"},
+            "response": [record.model_dump() for record in data],
+        },
+    )
+
+
+@router.get("/{id}", response_model=ResponseInterface[Profile])
+async def show(id: int, session: Annotated[Session, Depends(get_session)]):
+    data = session.get(Profile, id)
+    if not data:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={
+            "status": {"status_code": status.HTTP_200_OK, "message": "Records Found"},
+            "response": data.model_dump(),
+        },
+    )
