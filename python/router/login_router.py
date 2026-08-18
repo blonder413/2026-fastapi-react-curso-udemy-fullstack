32b35f61c353744a8f1b350e60a1069002e015ca
Jonathan Morales <blonder413@gmail.com>
2026-08-18 06:49:07 -0500

    fast-api(login): dto

--- python/router/login_router.py
+++ python/router/login_router.py
@@ -2,4 +2,6 @@ from fastapi import APIRouter, status, Depends, HTTPException
 from fastapi.responses import JSONResponse
 from typing import Annotated
 
+from .dto.login_dto import LoginDto
+
 router=APIRouter(prefix="/auth/login", tags=["Login"])
