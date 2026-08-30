683a1ce4d9834609c50cd5fc7664b1398aa9c0df
Jonathan Morales <blonder413@gmail.com>
2026-08-30 10:53:37 -0500

    fast-api: env file

--- node/src/pages/Home.tsx
+++ node/src/pages/Home.tsx
@@ -8,7 +8,7 @@ const Home = () => {
         <Header />
         <main className="content">
           <div className="container-fluid p-0">
-            <h1 className="h3 mb-3">Title</h1>
+            <h1 className="h3 mb-3">{`${import.meta.env.VITE_APP_NAME}`}</h1>
             <div className="row">Content</div>
           </div>
         </main>
