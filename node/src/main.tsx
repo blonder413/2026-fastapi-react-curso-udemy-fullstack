60078d7c3e620ed3a8d67a58aa22dfa94350c082
Jonathan Morales <blonder413@gmail.com>
2026-08-30 10:44:08 -0500

    fast-api: error 500 page

--- node/src/main.tsx
+++ node/src/main.tsx
@@ -4,13 +4,14 @@ import "/public/css/app.css";
 import Frontend from "./components/Frontend";
 import Home from "./pages/Home";
 import Error404 from "./pages/Error404";
+import Error500 from "./pages/Error500";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Frontend />,
     children: [
-      { index: true, element: <Home /> },
+      { index: true, element: <Home />, errorElement:<Error500 /> },
       { path: "*", element: <Error404 /> },
     ],
   },
