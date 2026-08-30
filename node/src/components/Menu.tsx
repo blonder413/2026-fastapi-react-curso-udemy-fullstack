3b19194585b9b9b95834addbb3a1452fc3cc9ea8
Jonathan Morales <blonder413@gmail.com>
2026-08-30 11:16:03 -0500

    react: Link instead a

--- node/src/components/Menu.tsx
+++ node/src/components/Menu.tsx
@@ -1,44 +1,46 @@
+import {Link} from "react-router-dom"
+
 const Menu = () => {
   return (
     <div id="sidebar" className="sidebar js-sidebar">
       <div className="sidebar-content js-simplebar">
-        <a href="" className="sidebar-brand">
+        <Link to="/" className="sidebar-brand">
           <span className="align-middle">
             <img src="/img/loguito.png" alt="logo" style={{ width: "150px" }} />
           </span>
-        </a>
+        </Link>
         <ul className="sidebar-nav">
           <li className="sidebar-header">Administración</li>
           <li className="sidebar-item">
-            <a href="" className="sidebar-link" title="Administrar perfiles">
+            <Link to="/perfiles" className="sidebar-link" title="Administrar perfiles">
               <i className="align-middle fas fa-list"></i>
               <span className="align-middle">Perfil</span>
-            </a>
+            </Link>
           </li>
           <li className="sidebar-item">
-            <a href="" className="sidebar-link" title="Administrar usuarios">
+            <Link to="/usuarios" className="sidebar-link" title="Administrar usuarios">
               <i className="align-middle fas fa-users"></i>
               <span className="align-middle">Usuarios</span>
-            </a>
+            </Link>
           </li>
           <li className="sidebar-item">
-            <a href="" className="sidebar-link" title="Administrar categorías">
+            <Link to="/negocios/categorias" className="sidebar-link" title="Administrar categorías">
               <i className="align-middle fas fa-list"></i>
               <span className="align-middle">Categorías</span>
-            </a>
+            </Link>
           </li>
           <li className="sidebar-item">
-            <a href="" className="sidebar-link" title="Administrar negocios">
+            <Link to="/negocios/listar" className="sidebar-link" title="Administrar negocios">
               <i className="align-middle fas fa-list-alt"></i>
               <span className="align-middle">Negocios</span>
-            </a>
+            </Link>
           </li>
           <li className="sidebar-header">Mi Negocio</li>
           <li className="sidebar-item">
-            <a href="" className="sidebar-link" title="Administrar mi negocio">
+            <Link to="/mi-negocio" className="sidebar-link" title="Administrar mi negocio">
               <i className="align-middle fas fa-list-alt"></i>
               <span className="align-middle">Mi Negocio</span>
-            </a>
+            </Link>
           </li>
 
         </ul>
