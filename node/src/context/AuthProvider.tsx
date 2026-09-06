d84d227425a70f352da1cdb40a7469a5002936a3
Jonathan Morales <blonder413@gmail.com>
2026-09-06 17:26:19 -0500

    react(login): save token

--- node/src/context/AuthProvider.tsx
+++ node/src/context/AuthProvider.tsx
@@ -5,9 +5,27 @@ import type { AuthProviderProps } from "../types/AuthProviderProps.type";
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 const AuthProvider = ({ children }: AuthProviderProps) => {
-  const [auth, setAuth] = useState<boolean>(true);
+  const [auth, setAuth] = useState<boolean>(
+    localStorage.getItem("menu_id") != null,
+  );
+
+  const handleLogin = (
+    id: string,
+    name: string,
+    token: string,
+    profile_id: string,
+  ) => {
+    localStorage.setItem("menu_id", id);
+    localStorage.setItem("menu_name", name);
+    localStorage.setItem("menu_token", token);
+    localStorage.setItem("menu_profile_id", profile_id);
+    setAuth(true);
+  };
+
   return (
-    <AuthContext.Provider value={{ auth }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ auth, handleLogin }}>
+      {children}
+    </AuthContext.Provider>
   );
 };
 
--- node/src/types/AutContext.type.ts
+++ node/src/types/AutContext.type.ts
@@ -1,3 +1,9 @@
 export type AuthContextType = {
   auth: boolean;
+  handleLogin: (
+    id: string,
+    name: string,
+    token: string,
+    profile_id: string,
+  ) => void;
 };
